732e7d2e5a2462df3d1050e12392f78a625d0b0e
Joniffer Mandac <101397206+joniffer10@users.noreply.github.com>
2023-08-04 18:43:17 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+    <title>E-learn</title>
+    <header>
+        <h1>Elearn</h1>
+        <div class="icons">
+            <a href="#"><i class="fa fa-info-circle">&nbsp; </i></a>
+            <a href="#"><i class="fa fa-power-off"></i></a>
+        </div>
+    </header>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <ul>
+        <li>
+            <a href="#">
+                <div class="icon">
+                    <i class="fa fa-folder-open-o"></i><br>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="icon">
+                    <i class="fa fa-globe"></i><br>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="icon">
+                    <i class="fa fa-gamepad"></i><br>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="icon">
+                    <i class="fa fa-gear"></i><br>
+                </div>
+            </a>
+        </li>
+
+    </ul>
+</body>
+</html>
